4d16b4989a6f78a0d9158c945e26a74cdbd0f585
Joqui2108 <joqui2108@hotmail.com>
2014-07-28 01:22:30 -0300

    Create V1_test.sh
    Primer versión sin probar

---
+++ V1_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+	then echo "Estás poniendo mal los archivos salame.";
+fi
+
+ffmpeg -loop 1 -i $1 -c:v h264 v1.mp4
+ffmpeg -i v1.mp4  -i $2 -strict experimental -c:v copy -c:a aac -b:a 384k video.mp4
+
+rm v1.mp4
+echo "Listo master"
