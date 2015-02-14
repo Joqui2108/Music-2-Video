5a3df4ab6bc2eb7d857a868466e834326720a947
Joqui2108 <Joqui2108@users.noreply.github.com>
2015-02-13 21:00:53 -0300

    Update V1_test.sh

--- V1_test.sh
+++ V1_test.sh
@@ -4,7 +4,7 @@ if [ $# -ne 2 ]
 	then echo "Estás poniendo mal los archivos salame.";
 fi
 
-ffmpeg -loop 1 -i $1 -c:v h264 v1.mp4
+ffmpeg -loop 1 -i $1 -c:v h264 -t 1 v1.mp4
 ffmpeg -i v1.mp4  -i $2 -strict experimental -c:v copy -c:a aac -b:a 384k video.mp4
 
 rm v1.mp4
