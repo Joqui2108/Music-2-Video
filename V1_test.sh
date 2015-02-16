b04661411d617c51a90ce336e00c2cc3e2d4e53b
Joqui2108 <Joqui2108@users.noreply.github.com>
2015-02-16 19:49:50 -0300

    Variables y sintaxis
    (Hay que probar) Ahora tal vez funciona bien todo.

--- V1_test.sh
+++ V1_test.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]
-	then echo "Estás poniendo mal los archivos salame.";
+command -v ffmpeg >/dev/null 2>&1 || { echo >&2 "Che, no está el ffmpeg instalado."; exit 1; }
+
+if [ "$#" -ne 2 ]; then
+	echo "Estás poniendo mal los archivos salame.";
 fi
 
-ffmpeg -loop 1 -i $1 -c:v h264 -t 1 v1.mp4
-ffmpeg -i v1.mp4  -i $2 -strict experimental -c:v copy -c:a aac -b:a 384k video.mp4
+COVER=$1
+FILE=$2
+
+ffmpeg -loop 1 -i $COVER -c:v h264 -t 1 v1.mp4
+ffmpeg -i v1.mp4  -i $FILE -strict experimental -c:v copy -c:a aac -b:a 384k video.mp4
 
 rm v1.mp4
 echo "Listo master"
